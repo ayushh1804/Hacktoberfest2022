04377afdf1e49d824c3ea79833ede929e0318c3d
AYUSH <88641651+ayushh1804@users.noreply.github.com>
2022-10-22 20:09:46 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2045,6 +2045,11 @@ contributors = [
     id:421,
     fullname:"Gautami SInha",
     username:"https://github.com/amnotcreative",
+  },
+  {
+    id:422,
+    fullname:"Ayush Raj",
+    username:"https://github.com/ayushh1804",
   }
 
 ];
